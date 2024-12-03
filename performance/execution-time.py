6e56b2c567cb5cbcb5bc4da82c73952a7648d67c
Felix Gust <gustifix@users.noreply.github.com>
2024-12-03 16:32:05 +0100

    Move csv files

--- performance/execution-time.py
+++ performance/execution-time.py
@@ -4,11 +4,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df_u50_300mhz = pd.read_csv("u50-300mhz.csv", skipinitialspace=True)
-df_u280_300mhz = pd.read_csv("u280-300mhz.csv", skipinitialspace=True)
-df_u50_faster = pd.read_csv("u50-faster.csv", skipinitialspace=True)
-df_u280_faster = pd.read_csv("u280-faster.csv", skipinitialspace=True)
-df_u280_ddr_300mhz = pd.read_csv("u280-ddr-300mhz.csv", skipinitialspace=True)
+df_u50_300mhz = pd.read_csv("../data/u50-perf-300mhz.csv", skipinitialspace=True)
+df_u280_300mhz = pd.read_csv("../data/u280-perf-300mhz.csv", skipinitialspace=True)
+df_u50_faster = pd.read_csv("../data/u50-perf-faster.csv", skipinitialspace=True)
+df_u280_faster = pd.read_csv("../data/u280-perf-faster.csv", skipinitialspace=True)
+df_u280_ddr_300mhz = pd.read_csv("../data/u280-perf-ddr-300mhz.csv", skipinitialspace=True)
 
 bar_width = 0.15
 app_names = df_u50_300mhz["app_name"].values
@@ -25,4 +25,4 @@ plt.ylabel("Average time (s)")
 plt.legend()
 plt.tight_layout()
 
-plt.savefig("performance.png")
+plt.savefig("execution-time.png")
