22c6fea31d05e965a198da1a4300382833fd2f55
Felix Gust <gustifix@users.noreply.github.com>
2024-12-19 09:57:57 +0100

    Add wip memory channel plots

---
+++ performance/memory-channels-kernel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_kernel_time = pd.read_csv("../data/memory-channels-kernel.csv", skipinitialspace=True)
+
+configs = ["singlechannel-300mhz", "multichannel-300mhz", "singlechannel-fast", "multichannel-fast",
+           "singlechannel-ddr-300mhz", "singlechannel-ddr-fast", "multichannel-ddr-300mhz", "multichannel-ddr-fast"]
+
+bar_width = 0.2
+app_names = df_kernel_time["app_name"].values
+x = np.arange(len(df_kernel_time))
+
+plt.bar(x - bar_width, df_kernel_time["kernel_avg_time"].values, width=bar_width, label="kernel")
+plt.bar(x, df_kernel_time["data_to_fpga_avg_time"].values, width=bar_width, label="data to fpga")
+plt.bar(x + bar_width, df_kernel_time["data_to_host_avg_time"].values, width=bar_width, label="data to host")
+
+plt.xticks(x, app_names, rotation=45, ha="right")
+plt.ylabel("Average time (ns)")
+# plt.ylim(top=12528700)
+plt.legend()
+plt.tight_layout()
+
+filename = "memory-channels-kernel.png"
+print(f"Saving figure to {filename}")
+plt.savefig(filename)
---
+++ performance/memory-channels.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_total_time = pd.read_csv("../data/memory-channels.csv", skipinitialspace=True)
+
+configs = ["singlechannel-300mhz", "multichannel-300mhz", "singlechannel-fast", "multichannel-fast",
+           "singlechannel-ddr-300mhz", "singlechannel-ddr-fast", "multichannel-ddr-300mhz", "multichannel-ddr-fast"]
+
+bar_width = 0.25
+app_names = df_total_time["app_name"].values
+x = np.arange(len(df_total_time))
+
+
+plt.bar(x, df_total_time["average"].values, width=bar_width)
+
+plt.xticks(x, app_names, rotation=45, ha="right")
+plt.ylabel("Average time (s)")
+plt.tight_layout()
+
+filename = "memory-channels.png"
+print(f"Saving figure to {filename}")
+plt.savefig(filename)
