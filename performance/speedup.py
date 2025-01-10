73308ef6d04189ac9051482c7342c12c472736b6
Felix Gust <gustifix@users.noreply.github.com>
2025-01-10 13:18:53 +0100

    Split speedup into 200mhz and unlimited

--- performance/speedup.py
+++ performance/speedup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 df_u50_slow = pd.read_csv("../data/u50-slow-vitis.csv", skipinitialspace=True)
 df_u50_fast = pd.read_csv("../data/u50-fast-vitis.csv", skipinitialspace=True)
@@ -11,32 +9,37 @@ df_u280_fast = pd.read_csv("../data/u280-fast-vitis.csv", skipinitialspace=True)
 df_u280_ddr_slow = pd.read_csv("../data/u280-ddr-slow-vitis.csv", skipinitialspace=True)
 df_u280_ddr_fast = pd.read_csv("../data/u280-ddr-fast-vitis.csv", skipinitialspace=True)
 
-dfs = [df_u50_slow, df_u50_fast, df_u280_slow, df_u280_fast, df_u280_ddr_slow, df_u280_ddr_fast]
-fpga_configs = ["u50_hbm_200mhz", "u50_hbm_unlimited", "u280_hbm_200mhz",
-                "u280_hbm_unlimited", "u280_ddr_200mhz", "u280_ddr_unlimited"]
+dfs_slow = [df_u50_slow, df_u280_slow, df_u280_ddr_slow]
+dfs_fast = [df_u50_fast, df_u280_fast, df_u280_ddr_fast]
+fpga_configs = ["u50_hbm", "u280_hbm", "u280_ddr"]
 
 # Sum up data transfer + kernel execution time
 for df in [df_u50_slow, df_u50_fast, df_u280_slow, df_u280_fast, df_u280_ddr_slow, df_u280_ddr_fast]:
-    df["transfer+kernel"] = df["data_to_fpga_average"] + df["kernel_average"] + df["data_to_host_average"]
+    df["transfer+kernel"] = df["data_to_fpga"] + df["kernel"] + df["data_to_host"]
     df["transfer+kernel"] /= 1_000_000 # to miliseconds
 
-df_combined = pd.concat(dfs, ignore_index=True)
 apps = df_u50_slow["app_name"].values
 num_apps = len(apps)
 
-print("app_name,slowest_time,slowest_fpga,fastest_time,fastest_fpga,speedup")
+filenames = ["speedup-200mhz.csv", "speedup-unlimited.csv"]
 
-for app in apps:
-    print(app, end=",")
-    idxmax = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].idxmax()
-    max = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].max()
-    max_fpga = fpga_configs[int(idxmax / num_apps)]
-    print(f"{max},{max_fpga},", end="")
+for i, dfs in enumerate([dfs_slow, dfs_fast]):
+    print(f"Saving data in {filenames[i]}")
+    f = open(filenames[i], "w")
+    f.write("app_name,slowest_time,slowest_fpga,fastest_time,fastest_fpga,speedup\n")
+    df_combined = pd.concat(dfs, ignore_index=True)
+    for app in apps:
+        f.write(f"{app},")
+        idxmax = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].idxmax()
+        print(idxmax)
+        max = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].max()
+        max_fpga = fpga_configs[int(idxmax / num_apps)]
+        f.write(f"{max},{max_fpga},")
 
-    idxmin = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].idxmin()
-    min = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].min()
-    min_fpga = fpga_configs[int(idxmin / num_apps)]
-    print(f"{min},{min_fpga},", end="")
+        idxmin = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].idxmin()
+        min = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].min()
+        min_fpga = fpga_configs[int(idxmin / num_apps)]
+        f.write(f"{min},{min_fpga},")
 
-    speedup = max / min
-    print(speedup)
+        speedup = max / min
+        f.write(f"{speedup}\n")
