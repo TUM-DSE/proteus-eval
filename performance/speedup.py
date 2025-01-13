dd9a42e446f436cf390a261bf92d2f919def17e1
Felix Gust <gustifix@users.noreply.github.com>
2025-01-13 15:25:37 +0100

    Add frequency only speedup

--- performance/speedup.py
+++ performance/speedup.py
@@ -8,6 +8,7 @@ df_u280_slow = pd.read_csv("../data/u280-slow-vitis.csv", skipinitialspace=True)
 df_u280_fast = pd.read_csv("../data/u280-fast-vitis.csv", skipinitialspace=True)
 df_u280_ddr_slow = pd.read_csv("../data/u280-ddr-slow-vitis.csv", skipinitialspace=True)
 df_u280_ddr_fast = pd.read_csv("../data/u280-ddr-fast-vitis.csv", skipinitialspace=True)
+df_freq = pd.read_csv("../data/frequencies.csv", skipinitialspace=True)
 
 dfs_slow = [df_u50_slow, df_u280_slow, df_u280_ddr_slow]
 dfs_fast = [df_u50_fast, df_u280_fast, df_u280_ddr_fast]
@@ -25,7 +26,7 @@ num_apps = len(apps)
 filenames = ["speedup-best-200mhz.csv", "speedup-best-unlimited.csv"]
 
 for i, dfs in enumerate([dfs_slow, dfs_fast]):
-    print(f"Saving data in {filenames[i]}")
+    print(f"Saving best case speedup data in {filenames[i]}")
     f = open(filenames[i], "w")
     f.write("app_name,slowest_time,slowest_fpga,fastest_time,fastest_fpga,speedup\n")
     df_combined = pd.concat(dfs, ignore_index=True)
@@ -43,3 +44,34 @@ for i, dfs in enumerate([dfs_slow, dfs_fast]):
 
         speedup = max / min
         f.write(f"{speedup}\n")
+
+# Speedup of bitstream with highest clock frequency ---------------------------------------------------------
+
+df_freq_fast = df_freq[["app_name", "u50_fast_freq", "u280_fast_freq", "u280_ddr_fast_freq"]]
+filename = "speedup-freq-unlimited.csv"
+
+print(f"Saving frequency only speedup data in {filename}")
+f = open(filename, "w")
+f.write("app_name,slowest_time,slowest_fpga,fastest_time,fastest_fpga,speedup\n")
+df_combined = pd.concat(dfs_fast, ignore_index=True)
+for app in apps:
+    f.write(f"{app},")
+    idxmax = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].idxmax()
+    max = df_combined.loc[df_combined["app_name"] == app, "transfer+kernel"].max()
+    max_fpga = fpga_configs[int(idxmax / num_apps)]
+    f.write(f"{max},{max_fpga},")
+
+    freq_max = df_freq_fast.loc[df_freq_fast["app_name"] == app, ["u50_fast_freq", "u280_fast_freq", "u280_ddr_fast_freq"]].idxmax(axis=1).iloc[0]
+    if freq_max == "u50_fast_freq":
+        freq_max_fpga = fpga_configs[0]
+        freq_max_time = df_u50_fast.loc[df_u50_fast["app_name"] == app, "transfer+kernel"].iloc[0]
+    elif freq_max == "u280_fast_freq":
+        freq_max_fpga = fpga_configs[1]
+        freq_max_time = df_u280_fast.loc[df_u280_fast["app_name"] == app, "transfer+kernel"].iloc[0]
+    elif freq_max == "u280_ddr_fast_freq":
+        freq_max_fpga = fpga_configs[2]
+        freq_max_time = df_u280_ddr_fast.loc[df_u280_ddr_fast["app_name"] == app, "transfer+kernel"].iloc[0]
+
+    f.write(f"{freq_max_time},{freq_max_fpga},")
+    speedup = max / freq_max_time
+    f.write(f"{speedup}\n")
