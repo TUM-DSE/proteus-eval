833f223066b3b4abe8afda4645f9a028bac42d51
Felix Gust <gustifix@users.noreply.github.com>
2025-02-24 13:16:23 +0100

    Add error bars to total time

--- scripts/time-compute.py
+++ scripts/time-compute.py
@@ -23,8 +23,11 @@ for setting in ["native", "proteus"]:
     df_u280_ddr_slow[setting] = pd.read_csv(f"../data/{setting}/u280-ddr-slow.csv", skipinitialspace=True).iloc[:10]
     df_u280_ddr_fast[setting] = pd.read_csv(f"../data/{setting}/u280-ddr-fast.csv", skipinitialspace=True).iloc[:10]
 
+    dfs = [df_u50_slow, df_u280_slow, df_u280_ddr_slow, df_u50_fast, df_u280_fast, df_u280_ddr_fast]
+    labels = ["U50 HBM 200 MHz", "U280 HBM 200 MHz", "U280 DDR 200 MHz", "U50 HBM unlimited", "U280 HBM unlimited", "U280 DDR unlimited"]
+
     # Sum up data transfer + kernel execution time, time_cpu seems to be measured incorrectly for Proteus currently
-    for df in [df_u50_slow, df_u50_fast, df_u280_slow, df_u280_fast, df_u280_ddr_slow, df_u280_ddr_fast]:
+    for df in dfs:
         df[setting]["transfer+kernel"] = df[setting]["data_to_fpga_ocl"] + df[setting]["kernel_ocl"] + df[setting]["data_to_host_ocl"]
 
     bar_width = 0.12
@@ -33,12 +36,10 @@ for setting in ["native", "proteus"]:
 
     # Total execution time --------------------------------------------------------------------------------------
 
-    plt.bar(x - 2 * bar_width, df_u50_slow[setting]["average"].values, width=bar_width, label=f"U50 HBM 200 MHz")
-    plt.bar(x - 1 * bar_width, df_u280_slow[setting]["average"].values, width=bar_width, label=f"U280 HBM 200 MHz")
-    plt.bar(x                , df_u280_ddr_slow[setting]["average"].values, width=bar_width, label=f"U280 DDR 200 MHz")
-    plt.bar(x + 1 * bar_width, df_u50_fast[setting]["average"].values, width=bar_width, label="U50 HBM unlimited")
-    plt.bar(x + 2 * bar_width, df_u280_fast[setting]["average"].values, width=bar_width, label="U280 HBM unlimited")
-    plt.bar(x + 3 * bar_width, df_u280_ddr_fast[setting]["average"].values, width=bar_width, label="U280 DDR unlimited")
+    for i in range(6):
+        x_offs = i - 2
+        plt.bar(x + x_offs * bar_width, dfs[i][setting]["average"].values, width=bar_width, label=labels[i])
+        plt.errorbar(x + x_offs * bar_width, dfs[i][setting]["average"].values, yerr=dfs[i][setting]["stddev"].values, fmt="none", color="k")
 
     plt.xticks(x, app_names, rotation=30, ha="right")
     plt.ylabel("Total execution time (s)")
@@ -52,12 +53,9 @@ for setting in ["native", "proteus"]:
 
     # Data transfer + kernel time -------------------------------------------------------------------------------
 
-    plt.bar(x - 2 * bar_width, df_u50_slow[setting]["transfer+kernel"].values, width=bar_width, label="U50 HBM 200 MHz")
-    plt.bar(x - 1 * bar_width, df_u280_slow[setting]["transfer+kernel"].values, width=bar_width, label="U280 HBM 200 MHz")
-    plt.bar(x                , df_u280_ddr_slow[setting]["transfer+kernel"].values, width=bar_width, label="U280 DDR 200 MHz")
-    plt.bar(x + 1 * bar_width, df_u50_fast[setting]["transfer+kernel"].values, width=bar_width, label="U50 HBM unlimited")
-    plt.bar(x + 2 * bar_width, df_u280_fast[setting]["transfer+kernel"].values, width=bar_width, label="U280 HBM unlimited")
-    plt.bar(x + 3 * bar_width, df_u280_ddr_fast[setting]["transfer+kernel"].values, width=bar_width, label="U280 DDR unlimited")
+    for i in range(6):
+        x_offs = i - 2
+        plt.bar(x + x_offs * bar_width, dfs[i][setting]["transfer+kernel"].values, width=bar_width, label=labels[i])
 
     plt.xticks(x, app_names, rotation=30, ha="right")
     plt.ylabel("Total data transfer + kernel time (s)")
@@ -74,15 +72,19 @@ for setting in ["native", "proteus"]:
 bar_width = 0.12
 app_names = df_u50_slow[setting]["app_name"].values
 x = np.arange(len(app_names))
+dfs = [df_u50_fast, df_u280_fast, df_u280_ddr_fast]
+labels = ["U50 HBM native", "U50 HBM Proteus", "U280 HBM native", "U280 HBM Proteus", "U280 DDR native", "U280 DDR Proteus"]
 
 # Total execution time --------------------------------------------------------------------------------------
 
-plt.bar(x - 2 * bar_width, df_u50_fast["native"]["average"].values, width=bar_width, label="U50 HBM native")
-plt.bar(x - 1 * bar_width, df_u50_fast["proteus"]["average"].values, width=bar_width, label="U50 HBM Proteus")
-plt.bar(x                , df_u280_fast["native"]["average"].values, width=bar_width, label="U280 HBM native")
-plt.bar(x + 1 * bar_width, df_u280_fast["proteus"]["average"].values, width=bar_width, label="U280 HBM Proteus")
-plt.bar(x + 2 * bar_width, df_u280_ddr_fast["native"]["average"].values, width=bar_width, label="U280 DDR native")
-plt.bar(x + 3 * bar_width, df_u280_ddr_fast["proteus"]["average"].values, width=bar_width, label="U280 DDR Proteus")
+x_offs = -2
+for i in range(3):
+    plt.bar(x + x_offs * bar_width, dfs[i]["native"]["average"].values, width=bar_width, label=labels[i])
+    plt.errorbar(x + x_offs * bar_width, dfs[i]["native"]["average"].values, yerr=dfs[i]["native"]["stddev"].values, fmt="none", color="k")
+    x_offs += 1
+    plt.bar(x + x_offs * bar_width, dfs[i]["proteus"]["average"].values, width=bar_width, label=labels[i + 1])
+    plt.errorbar(x + x_offs * bar_width, dfs[i]["proteus"]["average"].values, yerr=dfs[i]["proteus"]["stddev"].values, fmt="none", color="k")
+    x_offs += 1
 
 plt.xticks(x, app_names, rotation=30, ha="right")
 plt.ylabel("Total execution time (s)")
@@ -96,12 +98,12 @@ plt.clf()
 
 # Data transfer + kernel time -------------------------------------------------------------------------------
 
-plt.bar(x - 2 * bar_width, df_u50_fast["native"]["transfer+kernel"].values, width=bar_width, label="U50 HBM native")
-plt.bar(x - 1 * bar_width, df_u50_fast["proteus"]["transfer+kernel"].values, width=bar_width, label="U50 HBM Proteus")
-plt.bar(x                , df_u280_fast["native"]["transfer+kernel"].values, width=bar_width, label="U280 HBM native")
-plt.bar(x + 1 * bar_width, df_u280_fast["proteus"]["transfer+kernel"].values, width=bar_width, label="U280 HBM Proteus")
-plt.bar(x + 2 * bar_width, df_u280_ddr_fast["native"]["transfer+kernel"].values, width=bar_width, label="U280 DDR native")
-plt.bar(x + 3 * bar_width, df_u280_ddr_fast["proteus"]["transfer+kernel"].values, width=bar_width, label="U280 DDR Proteus")
+x_offs = -2
+for i in range(3):
+    plt.bar(x + x_offs * bar_width, dfs[i]["native"]["transfer+kernel"].values, width=bar_width, label=labels[i])
+    x_offs += 1
+    plt.bar(x + x_offs * bar_width, dfs[i]["proteus"]["transfer+kernel"].values, width=bar_width, label=labels[i + 1])
+    x_offs += 1
 
 plt.xticks(x, app_names, rotation=30, ha="right")
 plt.ylabel("Total data transfer + kernel time (s)")
