2f514efa15b86de497107f85fdc7977350ae24ed
Felix Gust <gustifix@users.noreply.github.com>
2025-03-14 16:04:43 +0100

    Add DDR dual channel to oversub.py (proper dual channel data missing)

--- scripts/oversub.py
+++ scripts/oversub.py
@@ -7,12 +7,14 @@ import matplotlib.pyplot as plt
 
 df_u280_fast = pd.read_csv(f"../data/native/oversub-u280-fast.csv", skipinitialspace=True)
 df_u280_ddr_fast = pd.read_csv(f"../data/native/oversub-u280-ddr-fast.csv", skipinitialspace=True)
+df_u280_ddr_dc_fast = pd.read_csv(
+    f"../data/native/oversub-u280-ddr-dualchannel-fast.csv", skipinitialspace=True)
 dfs = [df_u280_fast, df_u280_ddr_fast]
 
-# Runs without -o option until this index
+# Runs for HBM and DDR without -o option until this index, DDR dual channel always uses -o option
 unopt_index = 8
 
-bar_width = 0.2
+bar_width = 0.15
 app_names = df_u280_fast["mem_limit"][:unopt_index]
 app_names[0] = "unlimited"
 x = np.arange(len(app_names))
@@ -35,29 +37,45 @@ times_ddr_opt = np.insert(times_ddr_opt, 0, 0.0)
 stddev_ddr_opt = np.insert(stddev_ddr_opt, 0, 0.0)
 ddr_opt_diff = (times_ddr_opt[1:] / times_ddr_unopt[1:] * 100) - 100
 
+times_ddr_dc = df_u280_ddr_dc_fast["time_total"].values
+stddev_ddr_dc = df_u280_ddr_dc_fast["time_total_stddev"].values
+# Index 0 is no mem limit, no chunking, no optimization
+times_ddr_dc = np.insert(times_ddr_dc, 0, 0.0)
+stddev_ddr_dc = np.insert(stddev_ddr_dc, 0, 0.0)
+ddr_dc_diff = (times_ddr_dc[1:] / times_ddr_opt[1:] * 100) - 100
+
 # HBM unopt
-plt.bar(x - 1.5 * bar_width, times_hbm_unopt, width=bar_width, label="HBM sequential")
-plt.errorbar(x - 1.5 * bar_width, times_hbm_unopt, yerr=stddev_hbm_unopt, fmt="none", color="k")
+plt.bar(x - 2 * bar_width, times_hbm_unopt, width=bar_width, label="HBM sequential")
+plt.errorbar(x - 2 * bar_width, times_hbm_unopt, yerr=stddev_hbm_unopt, fmt="none", color="k")
 
 # HBM opt
-bars = plt.bar(x - 0.5 * bar_width, times_hbm_opt, width=bar_width, label="HBM optimized")
+bars = plt.bar(x - 1 * bar_width, times_hbm_opt, width=bar_width, label="HBM optimized")
 for i, b in enumerate(bars[1:]):
-    plt.text(b.get_x() + 0.04, b.get_height() + 0.05,
+    plt.text(b.get_x() + 0.02, b.get_height() + 0.05,
              f"{hbm_opt_diff[i]:+.2f}%", rotation=90, size=7)
-plt.errorbar(x - 0.5 * bar_width, times_hbm_opt, yerr=stddev_hbm_opt, fmt="none", color="k")
+plt.errorbar(x - 1 * bar_width, times_hbm_opt, yerr=stddev_hbm_opt, fmt="none", color="k")
 
 # DDR unopt
-plt.bar(x + 0.5 * bar_width, times_ddr_unopt, width=bar_width,
+plt.bar(x + 0 * bar_width, times_ddr_unopt, width=bar_width,
         label="DDR sequential, unified memory banks")
-plt.errorbar(x + 0.5 * bar_width, times_ddr_unopt, yerr=stddev_ddr_unopt, fmt="none", color="k")
+plt.errorbar(x + 0 * bar_width, times_ddr_unopt, yerr=stddev_ddr_unopt, fmt="none", color="k")
 
 # DDR opt
-bars = plt.bar(x + 1.5 * bar_width, times_ddr_opt, width=bar_width,
+bars = plt.bar(x + 1 * bar_width, times_ddr_opt, width=bar_width,
                label="DDR optimized, unified memory banks")
 for i, b in enumerate(bars[1:]):
-    plt.text(b.get_x() + 0.04, b.get_height() + 0.05,
+    plt.text(b.get_x() + 0.02, b.get_height() + 0.05,
              f"{ddr_opt_diff[i]:+.2f}%", rotation=90, size=7)
-plt.errorbar(x + 1.5 * bar_width, times_ddr_opt, yerr=stddev_ddr_opt, fmt="none", color="k")
+plt.errorbar(x + 1 * bar_width, times_ddr_opt, yerr=stddev_ddr_opt, fmt="none", color="k")
+
+# DDR opt + dualchannel
+bars = plt.bar(x + 2 * bar_width, times_ddr_dc, width=bar_width,
+               label="DDR optimized, separate memory banks")
+for i, b in enumerate(bars[1:]):
+    plt.text(b.get_x() + 0.02, b.get_height() + 0.05,
+             f"{ddr_dc_diff[i]:+.2f}%", rotation=90, size=7)
+plt.errorbar(x + 2 * bar_width, times_ddr_dc, yerr=stddev_ddr_dc, fmt="none", color="k")
+
 
 plt.xticks(x, app_names)
 plt.xlabel("On-FPGA memory usage (MiB)")
