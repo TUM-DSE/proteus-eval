bb36d97dffc7376a2af927fe08d38d5cf80a3ec2
Felix Gust <gustifix@users.noreply.github.com>
2025-03-20 13:38:25 +0100

    Remove programming time from overheads

--- scripts/overheads.py
+++ scripts/overheads.py
@@ -5,14 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv(f"../data/overheads.csv", skipinitialspace=True)
-times = df[["program_bs", "kernel_alloc", "kernel_setarg",
-            "kernel_enqueue", "buf_alloc", "transfer", "finish"]]
-times_stddevs = df[["program_bs_stddev", "kernel_alloc_stddev", "kernel_setarg_stddev",
+times = df[["kernel_alloc", "kernel_setarg", "kernel_enqueue", "buf_alloc", "transfer", "finish"]]
+times_stddevs = df[["kernel_alloc_stddev", "kernel_setarg_stddev",
                     "kernel_enqueue_stddev", "buf_alloc_stddev", "transfer_stddev", "finish_stddev"]]
 
 bar_width = 0.12
-x_labels = ["Program bitstream", "Allocate kernel", "Set kernel args",
-            "Enqueue kernel", "Allocate buffer", "Data transfer", "Finish"]
+x_labels = ["Allocate kernel", "Set kernel args", "Enqueue kernel", "Allocate buffer", "Data transfer", "Finish"]
 x = np.arange(len(x_labels))
 
 plt.bar(x - 2.5 * bar_width, times.iloc[0], width=bar_width, label="Proteus U50")
