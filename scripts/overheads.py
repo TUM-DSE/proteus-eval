19d461ab6d31485b967be8b53b03ef906a0ff43c
Felix Gust <gustifix@users.noreply.github.com>
2025-04-15 16:32:52 +0200

    Update overheads style

--- scripts/overheads.py
+++ scripts/overheads.py
@@ -10,6 +10,20 @@ times_stddevs = df[["kernel_alloc_stddev", "kernel_setarg_stddev",
                     "kernel_enqueue_stddev", "buf_alloc_stddev", "transfer_stddev", "finish_stddev"]]
 
 bar_width = 0.12
+
+bar_args = {
+    "width": bar_width,
+    "linewidth": 1,
+    "edgecolor": "k",
+}
+
+errorbar_args = {
+    "fmt": "none",
+    "color": "k",
+    "elinewidth": 1,
+    "capsize": 2,
+}
+
 plt.rcParams.update({'font.size': 12})
 width = 7.0
 aspect = 1.6
@@ -19,23 +33,23 @@ plt.figure(figsize=(width, height))
 x_labels = ["Allocate kernel", "Set kernel args", "Enqueue kernel", "Allocate buffer", "Data transfer", "Finish"]
 x = np.arange(len(x_labels))
 
-plt.bar(x - 2.5 * bar_width, times.iloc[0], width=bar_width, label="Proteus U50")
-plt.errorbar(x - 2.5 * bar_width, times.iloc[0], yerr=times_stddevs.iloc[0], fmt="none", color="k")
+plt.bar(x - 2.5 * bar_width, times.iloc[0], label="Proteus U50", **bar_args)
+plt.errorbar(x - 2.5 * bar_width, times.iloc[0], yerr=times_stddevs.iloc[0], **errorbar_args)
 
-plt.bar(x - 1.5 * bar_width, times.iloc[3], width=bar_width, label="Native U50")
-plt.errorbar(x - 1.5 * bar_width, times.iloc[3], yerr=times_stddevs.iloc[3], fmt="none", color="k")
+plt.bar(x - 1.5 * bar_width, times.iloc[3], label="Native U50", **bar_args)
+plt.errorbar(x - 1.5 * bar_width, times.iloc[3], yerr=times_stddevs.iloc[3], **errorbar_args)
 
-plt.bar(x - 0.5 * bar_width, times.iloc[1], width=bar_width, label="Proteus U280 HBM")
-plt.errorbar(x - 0.5 * bar_width, times.iloc[1], yerr=times_stddevs.iloc[1], fmt="none", color="k")
+plt.bar(x - 0.5 * bar_width, times.iloc[1], label="Proteus U280 HBM", **bar_args)
+plt.errorbar(x - 0.5 * bar_width, times.iloc[1], yerr=times_stddevs.iloc[1], **errorbar_args)
 
-plt.bar(x + 0.5 * bar_width, times.iloc[4], width=bar_width, label="Native U280 HBM")
-plt.errorbar(x + 0.5 * bar_width, times.iloc[4], yerr=times_stddevs.iloc[4], fmt="none", color="k")
+plt.bar(x + 0.5 * bar_width, times.iloc[4], label="Native U280 HBM", **bar_args)
+plt.errorbar(x + 0.5 * bar_width, times.iloc[4], yerr=times_stddevs.iloc[4], **errorbar_args)
 
-plt.bar(x + 1.5 * bar_width, times.iloc[2], width=bar_width, label="Proteus U280 DDR")
-plt.errorbar(x + 1.5 * bar_width, times.iloc[2], yerr=times_stddevs.iloc[2], fmt="none", color="k")
+plt.bar(x + 1.5 * bar_width, times.iloc[2], label="Proteus U280 DDR", **bar_args)
+plt.errorbar(x + 1.5 * bar_width, times.iloc[2], yerr=times_stddevs.iloc[2], **errorbar_args)
 
-plt.bar(x + 2.5 * bar_width, times.iloc[5], width=bar_width, label="Native U280 DDR")
-plt.errorbar(x + 2.5 * bar_width, times.iloc[5], yerr=times_stddevs.iloc[5], fmt="none", color="k")
+plt.bar(x + 2.5 * bar_width, times.iloc[5], label="Native U280 DDR", **bar_args)
+plt.errorbar(x + 2.5 * bar_width, times.iloc[5], yerr=times_stddevs.iloc[5], **errorbar_args)
 
 plt.xticks(x, x_labels, rotation=30, ha="right")
 plt.ylabel("Time (ms)")
