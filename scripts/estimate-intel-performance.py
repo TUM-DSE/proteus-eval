7a492200cd2337f9de97a902b9791564dccf9a9c
Felix Gust <gustifix@users.noreply.github.com>
2025-04-28 15:16:17 +0200

    Add kernel time estimation for intel

--- scripts/estimate-intel-performance.py
+++ scripts/estimate-intel-performance.py
@@ -34,7 +34,8 @@ df_s10_est["app_name"] = app_names
 
 # Time without fpga = average - data_to_fpga_ocl - kernel_ocl - data_to_host_ocl
 
-average_without_fpga = df_s10["average"] - df_s10["data_to_fpga_ocl"] - df_s10["kernel_ocl"] - df_s10["data_to_host_ocl"]
+average_without_fpga = df_s10["average"] - df_s10["data_to_fpga_ocl"] - \
+    df_s10["kernel_ocl"] - df_s10["data_to_host_ocl"]
 print("Average without FPGA:")
 print(average_without_fpga)
 
@@ -47,9 +48,14 @@ print("to host")
 print(est_data_to_host)
 
 # s10_kernel_time = u280_kernel_time * (u280_freq / s10_freq)
-freq_ratios = df_freq["u280_ddr_fast_freq"]/df_freq["s10_fast_freq"]
+freq_ratios = df_freq["u280_ddr_fast_freq"] / df_freq["s10_fast_freq"]
 print("freq ratios")
 print(freq_ratios)
+est_kernel = df_u280_ddr_fast["kernel_ocl"] * freq_ratios
+print("est kernel")
+print(est_kernel)
+print("U280 kernel")
+print(df_u280_ddr_fast["kernel_ocl"])
 
 df_s10_est["average"] = 0.0
 df_s10_est["stddev"] = 0.0
@@ -60,7 +66,7 @@ df_s10_est["time_cpu"] = 0.0
 df_s10_est["time_cpu_stddev"] = 0.0
 df_s10_est["data_to_fpga_ocl"] = est_data_to_fpga
 df_s10_est["data_to_fpga_ocl_stddev"] = 0.0
-df_s10_est["kernel_ocl"] = 0.0
+df_s10_est["kernel_ocl"] = est_kernel
 df_s10_est["kernel_ocl_stddev"] = 0.0
 df_s10_est["data_to_host_ocl"] = est_data_to_host
 df_s10_est["data_to_host_ocl_stddev"] = 0.0
