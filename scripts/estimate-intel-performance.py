66d684639639cf7fd5feb7c35d149416f592f049
Felix Gust <gustifix@users.noreply.github.com>
2025-04-28 15:11:12 +0200

    Add data transfer estimation for intel

--- scripts/estimate-intel-performance.py
+++ scripts/estimate-intel-performance.py
@@ -22,17 +22,34 @@ df_freq = pd.read_csv(f"{common.data_rootdir}/frequencies.csv")
 app_names = df_s10["app_name"].values
 
 mask = df_u280_ddr_fast["app_name"].isin(app_names)
-df_u280_ddr_fast = df_u280_ddr_fast[mask]
+df_u280_ddr_fast = df_u280_ddr_fast[mask].reset_index(drop=True)
 print(df_u280_ddr_fast)
 
 mask = df_freq["app_name"].isin(app_names)
-df_freq = df_freq[mask]
+df_freq = df_freq[mask].reset_index(drop=True)
 print(df_freq)
 
 df_s10_est = pd.DataFrame()
 df_s10_est["app_name"] = app_names
 
 # Time without fpga = average - data_to_fpga_ocl - kernel_ocl - data_to_host_ocl
+
+average_without_fpga = df_s10["average"] - df_s10["data_to_fpga_ocl"] - df_s10["kernel_ocl"] - df_s10["data_to_host_ocl"]
+print("Average without FPGA:")
+print(average_without_fpga)
+
+# Use U280 values for data transfer time estimation
+est_data_to_fpga = df_u280_ddr_fast["data_to_fpga_ocl"]
+print("to fpga")
+print(est_data_to_fpga)
+est_data_to_host = df_u280_ddr_fast["data_to_host_ocl"]
+print("to host")
+print(est_data_to_host)
+
+# s10_kernel_time = u280_kernel_time * (u280_freq / s10_freq)
+freq_ratios = df_freq["u280_ddr_fast_freq"]/df_freq["s10_fast_freq"]
+print("freq ratios")
+print(freq_ratios)
 
 df_s10_est["average"] = 0.0
 df_s10_est["stddev"] = 0.0
@@ -41,11 +58,11 @@ df_s10_est["kernel_output_data_size"] = df_s10["kernel_output_data_size"]
 df_s10_est["kernel_iterations"] = df_s10["kernel_iterations"]
 df_s10_est["time_cpu"] = 0.0
 df_s10_est["time_cpu_stddev"] = 0.0
-df_s10_est["data_to_fpga_ocl"] = 0.0
+df_s10_est["data_to_fpga_ocl"] = est_data_to_fpga
 df_s10_est["data_to_fpga_ocl_stddev"] = 0.0
 df_s10_est["kernel_ocl"] = 0.0
 df_s10_est["kernel_ocl_stddev"] = 0.0
-df_s10_est["data_to_host_ocl"] = 0.0
+df_s10_est["data_to_host_ocl"] = est_data_to_host
 df_s10_est["data_to_host_ocl_stddev"] = 0.0
 
 print(df_s10_est)
