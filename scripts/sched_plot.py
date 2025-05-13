ed135efd2d4a6b842f6f5c9d909f298e657512dd
Atsushi Koshiba <cos24boc@gmail.com>
2025-05-13 16:34:55 +0200

    sched_plot: separate Rosetta apps

--- scripts/sched_plot.py
+++ scripts/sched_plot.py
@@ -55,6 +55,10 @@ print(app_names)
 
 mem_app_patterns = ["wide_mem_rw", "gmem_2banks"]
 
+mem_app_patterns = ["wide_mem_rw", "gmem_2banks"]
+
+ros_app_names = ["3d-rendering", "digit-recognition", "optical-flow", "spam-filter"]
+
 # Get app dfs
 dfs = []
 dfs_mem = []
@@ -98,6 +102,7 @@ print("----------Worst----------")
 worst_rows = []
 worst_cmp_rows = []
 worst_mem_rows = []
+worst_ros_rows = []
 for i, df_app in enumerate(dfs):
     idx = df_app["thrp"].idxmin()
     worst_rows.append(df_app.iloc[[idx]])
@@ -105,6 +110,8 @@ for i, df_app in enumerate(dfs):
     # For separate plots
     if any(app_names[i] == mem_app for mem_app in mem_app_names):
         worst_mem_rows.append(df_app.iloc[[idx]])
+    elif any(app_names[i] == ros_app for ros_app in ros_app_names):
+        worst_ros_rows.append(df_app.iloc[[idx]])
     else:
         worst_cmp_rows.append(df_app.iloc[[idx]])
 
@@ -114,13 +121,16 @@ df_worst = pd.concat(worst_rows, ignore_index=True)
 # dfs for separate plots
 df_worst_cmp = pd.concat(worst_cmp_rows, ignore_index=True)
 df_worst_mem = pd.concat(worst_mem_rows, ignore_index=True)
+df_worst_ros = pd.concat(worst_ros_rows, ignore_index=True)
 print(df_worst_cmp)
 print(df_worst_mem)
+print(df_worst_ros)
 
 # Best case
 best_rows = []
 best_cmp_rows = []
 best_mem_rows = []
+best_ros_rows = []
 print("----------Best-----------")
 for i, df_app in enumerate(dfs):
     idx = df_app["thrp"].idxmax()
@@ -129,6 +139,8 @@ for i, df_app in enumerate(dfs):
     # For separate plots
     if any(app_names[i] == mem_app for mem_app in mem_app_names):
         best_mem_rows.append(df_app.iloc[[idx]])
+    elif any(app_names[i] == ros_app for ros_app in ros_app_names):
+        best_ros_rows.append(df_app.iloc[[idx]])
     else:
         best_cmp_rows.append(df_app.iloc[[idx]])
 
@@ -138,14 +150,17 @@ df_best = pd.concat(best_rows, ignore_index=True)
 # dfs for separate plots
 df_best_cmp = pd.concat(best_cmp_rows, ignore_index=True)
 df_best_mem = pd.concat(best_mem_rows, ignore_index=True)
+df_best_ros = pd.concat(best_ros_rows, ignore_index=True)
 print(df_best_cmp)
 print(df_best_mem)
+print(df_best_ros)
     
 # Proteus (freq-only)
 print("----------Freq-----------")
 freq_rows = []
 freq_cmp_rows = []
 freq_mem_rows = []
+freq_ros_rows = []
 for i, df_app in enumerate(dfs):
     # choose the FPGA(s) with the highest (best) score
     best_score = df_score_freq[app_names[i]].max()
@@ -160,6 +175,8 @@ for i, df_app in enumerate(dfs):
     # For separate plots
     if any(app_names[i] == mem_app for mem_app in mem_app_names):
         freq_mem_rows.append(df_app.iloc[[idx]])
+    elif any(app_names[i] == ros_app for ros_app in ros_app_names):
+        freq_ros_rows.append(df_app.iloc[[idx]])
     else:
         freq_cmp_rows.append(df_app.iloc[[idx]])
 
@@ -169,14 +186,17 @@ df_freq = pd.concat(freq_rows, ignore_index=True)
 # dfs for separate plots
 df_freq_cmp = pd.concat(freq_cmp_rows, ignore_index=True)
 df_freq_mem = pd.concat(freq_mem_rows, ignore_index=True)
+df_freq_ros = pd.concat(freq_ros_rows, ignore_index=True)
 print(df_freq_cmp)
 print(df_freq_mem)
+print(df_freq_ros)
 
 # Proteus 
 print("---------Proteus---------")
 proteus_rows = []
 proteus_cmp_rows = []
 proteus_mem_rows = []
+proteus_ros_rows = []
 for i, df_app in enumerate(dfs):
     # choose the FPGA(s) with the highest (best) score
     best_score = df_score_new[app_names[i]].min()
@@ -191,6 +211,8 @@ for i, df_app in enumerate(dfs):
     # For separate plots
     if any(app_names[i] == mem_app for mem_app in mem_app_names):
         proteus_mem_rows.append(df_app.iloc[[idx]])
+    elif any(app_names[i] == ros_app for ros_app in ros_app_names):
+        proteus_ros_rows.append(df_app.iloc[[idx]])
     else:
         proteus_cmp_rows.append(df_app.iloc[[idx]])
 
@@ -200,12 +222,15 @@ df_proteus = pd.concat(proteus_rows, ignore_index=True)
 # dfs for separate plots
 df_proteus_cmp = pd.concat(proteus_cmp_rows, ignore_index=True)
 df_proteus_mem = pd.concat(proteus_mem_rows, ignore_index=True)
+df_proteus_ros = pd.concat(proteus_ros_rows, ignore_index=True)
 print(df_proteus_cmp)
 print(df_proteus_mem)
+print(df_proteus_ros)
 
 dfs_plot = [df_worst, df_best, df_freq, df_proteus]
 dfs_plot_mem = [df_worst_mem, df_best_mem, df_freq_mem, df_proteus_mem]
 dfs_plot_cmp = [df_worst_cmp, df_best_cmp, df_freq_cmp, df_proteus_cmp]
+dfs_plot_ros = [df_worst_ros, df_best_ros, df_freq_ros, df_proteus_ros]
 labels = ["Worst", "Best", "Proteus (Fmax)", "Proteus"]
 # labels = ["Best", "Worst", "Proteus"]
           # "Freq-only(2nd)", "Proteus", "Proteus(2nd)"]
@@ -223,9 +248,14 @@ best_improve_mem    = 100.0 * (df_best_mem['thrp']    - df_worst_mem['thrp']) / 
 freq_improve_mem    = 100.0 * (df_freq_mem['thrp']    - df_worst_mem['thrp']) / df_worst_mem['thrp']
 proteus_improve_mem = 100.0 * (df_proteus_mem['thrp'] - df_worst_mem['thrp']) / df_worst_mem['thrp']
 
+best_improve_ros    = 100.0 * (df_best_ros['thrp']    - df_worst_ros['thrp']) / df_worst_ros['thrp']
+freq_improve_ros    = 100.0 * (df_freq_ros['thrp']    - df_worst_ros['thrp']) / df_worst_ros['thrp']
+proteus_improve_ros = 100.0 * (df_proteus_ros['thrp'] - df_worst_ros['thrp']) / df_worst_ros['thrp']
+
 improves = [best_improve, freq_improve, proteus_improve]
 improves_cmp = [best_improve_cmp, freq_improve_cmp, proteus_improve_cmp]
 improves_mem = [best_improve_mem, freq_improve_mem, proteus_improve_mem]
+improves_ros = [best_improve_ros, freq_improve_ros, proteus_improve_ros]
 
 print(f"Best          : {best_improve.mean()}")
 print(f"Proteus (freq): {freq_improve.mean()}")
@@ -238,8 +268,9 @@ width = 14.0
 height = 3.5
 # aspect = 4.0
 # height = width / aspect
-width_cmp = width * (13.3/18)
-width_mem = width * (4.7/18)
+width_cmp = width * (8.8/18)
+width_mem = width * (4.6/18)
+width_ros = width * (4.6/18)
 
 
 colors = [common.bar_blue, common.bar_orange, common.bar_green, common.bar_green]
@@ -248,12 +279,15 @@ hatches = ["", "", "", "//"]
 xlabel_names = []
 xlabel_names_mem = []
 xlabel_names_cmp = []
+xlabel_names_ros = []
 mem_app_num = 0
 for app in app_names:
     xlabel_names.append(common.app_names_abb[app])
     # For separate plots
     if any(app == mem_app for mem_app in mem_app_names):
         xlabel_names_mem.append(common.app_names_abb[app])
+    elif any(app == ros_app for ros_app in ros_app_names):
+        xlabel_names_ros.append(common.app_names_abb[app])
     else:
         xlabel_names_cmp.append(common.app_names_abb[app])
 
@@ -295,7 +329,6 @@ plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
 plt.rcParams.update({'font.size': 12})
 plt.figure(figsize=(width_cmp, height))
 
-# x = np.arange(len(app_names)-mem_app_num)
 x2 = np.arange(len(xlabel_names_cmp))
 for i in range(len(dfs_plot_cmp)):
     x_offs = i - 1.5
@@ -312,9 +345,9 @@ for i in range(len(dfs_plot_cmp)):
                  f"  {improves_cmp[i-1][j]:.1f}%", rotation=90, size=8.5)
 
 plt.xticks(x2, xlabel_names_cmp, rotation=12)
-plt.ylim(0,2.8)
+plt.ylim(0,2.6)
 plt.ylabel("Throughput (GiB/s)")
-plt.legend(loc='upper left', fancybox=True, shadow=True, ncol=4, prop={'size': 11}, bbox_to_anchor=(0.28, 1.08))
+plt.legend(loc='upper right', fancybox=True, shadow=True, ncol=2, prop={'size': 11}, bbox_to_anchor=(1.0, 1.09))
 plt.tight_layout()
 configure_ax()
 
@@ -329,7 +362,6 @@ plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
 plt.rcParams.update({'font.size': 12})
 plt.figure(figsize=(width_mem, height))
 
-# x = np.arange(len(app_names)-mem_app_num)
 x3 = np.arange(len(xlabel_names_mem))
 for i in range(len(dfs_plot_mem)):
     x_offs = i - 1.5
@@ -356,5 +388,39 @@ filename_mem = f"../plots/scheduler-thrp-mem.pdf"
 print(f"Saving figure to {filename_mem}")
 plt.margins(x=0.01, tight=True)
 plt.savefig(filename_mem, **savefig_args)
+plt.clf()
+
+# Total throughput (Rosetta)  --------------------------------------------------------------------------------------
+plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
+plt.rcParams.update({'font.size': 12})
+plt.figure(figsize=(width_ros, height))
+
+x3 = np.arange(len(xlabel_names_ros))
+for i in range(len(dfs_plot_ros)):
+    x_offs = i - 1.5
+    print(f"position: {x3}, {x3+x_offs*bar_width}")
+    bars = plt.bar(x3 + x_offs * bar_width, dfs_plot_ros[i]["thrp"].values,
+            hatch=hatches[i], label=labels[i], **bar_args)
+
+    # skip the worst case (no numbers)
+    if i == 0: 
+        continue
+
+    for j, b in enumerate(bars):
+        plt.text(b.get_x() + 0.19 * bar_width, b.get_height(),
+                 f"  {improves_ros[i-1][j]:.1f}%", rotation=90, size=8.5)
+
+plt.xticks(x3, xlabel_names_ros, rotation=12)
+plt.ylim(0,2.6)
+# plt.ylim(0,8.0)
+plt.ylabel("Throughput (GiB/s)")
+# plt.legend(loc='upper left', fancybox=True, shadow=True, ncol=4, bbox_to_anchor=(0.0, 1.1))
+plt.tight_layout()
+configure_ax()
+
+filename_ros = f"../plots/scheduler-thrp-ros.pdf"
+print(f"Saving figure to {filename_ros}")
+plt.margins(x=0.01, tight=True)
+plt.savefig(filename_ros, **savefig_args)
 plt.clf()
 
