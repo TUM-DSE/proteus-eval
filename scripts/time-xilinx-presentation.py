e0e5e035380bb677eaae77edcf6c2e1418c2c90a
Felix Gust <gustifix@users.noreply.github.com>
2026-02-03 13:15:47 +0100

    Add performance plot for presentation

---
+++ scripts/time-xilinx-presentation.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics as stat
+import common
+
+
+def configure_ax():
+    ax = plt.gca()
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    # ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    ax.set_axisbelow(True)
+    ax.grid(axis='y')
+
+
+df_u50_slow = {}
+df_u50_fast = {}
+df_u280_slow = {}
+df_u280_fast = {}
+df_u280_ddr_slow = {}
+df_u280_ddr_fast = {}
+
+bar_width = 0.2
+
+bar_args = {
+    "width": bar_width,
+    "linewidth": 1,
+    "edgecolor": "k",
+}
+
+errorbar_args = {
+    "fmt": "none",
+    "color": "k",
+    "elinewidth": 1,
+    "capsize": 2,
+}
+
+savefig_args = {
+    "dpi": 300,
+    "pad_inches": 0.02,
+    "bbox_inches": 'tight',
+    "format": "pdf",
+}
+
+colors = [common.bar_blue, common.bar_blue, common.bar_orange,
+          common.bar_orange, common.bar_green, common.bar_green]
+hatches = ["", "//"]
+plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
+
+plt.rcParams.update({'font.size': 12})
+width = 10.0
+# aspect = 5
+# height = width / aspect
+height = 2.4
+plt.figure(figsize=(width, height))
+
+# Individual plots ------------------------------------------------------------------------------------------
+
+for setting in ["native", "proteus"]:
+
+    # First 12 rows are the Vitis Accel Examples
+    df_u50_slow[setting] = pd.read_csv(
+        f"../data/{setting}/u50-slow.csv", skipinitialspace=True).iloc[:13]
+    df_u50_fast[setting] = pd.read_csv(
+        f"../data/{setting}/u50-fast.csv", skipinitialspace=True).iloc[:13]
+    df_u280_slow[setting] = pd.read_csv(
+        f"../data/{setting}/u280-slow.csv", skipinitialspace=True).iloc[:13]
+    df_u280_fast[setting] = pd.read_csv(
+        f"../data/{setting}/u280-fast.csv", skipinitialspace=True).iloc[:13]
+    df_u280_ddr_slow[setting] = pd.read_csv(
+        f"../data/{setting}/u280-ddr-slow.csv", skipinitialspace=True).iloc[:13]
+    df_u280_ddr_fast[setting] = pd.read_csv(
+        f"../data/{setting}/u280-ddr-fast.csv", skipinitialspace=True).iloc[:13]
+
+    dfs = [df_u50_slow, df_u280_slow, df_u280_ddr_slow, df_u50_fast, df_u280_fast, df_u280_ddr_fast]
+    labels = ["U50 HBM 200 MHz", "U280 HBM 200 MHz", "U280 DDR 200 MHz",
+              "U50 HBM unlimited", "U280 HBM unlimited", "U280 DDR unlimited"]
+
+    # Sum up data transfer + kernel execution time,
+    # time_cpu seems to be measured incorrectly for Proteus currently
+    for i in range(len(dfs)):
+        dfs[i][setting]["transfer+kernel"] = dfs[i][setting]["data_to_fpga_ocl"] + \
+            dfs[i][setting]["kernel_ocl"] + dfs[i][setting]["data_to_host_ocl"]
+        # Average standard deviation is the square root of the average variance, variance is stddev ** 2
+        avg_variance = (dfs[i][setting]["data_to_fpga_ocl_stddev"] ** 2 + dfs[i][setting]
+                        ["kernel_ocl_stddev"] ** 2 + dfs[i][setting]["data_to_host_ocl_stddev"] ** 2) / 3
+        dfs[i][setting]["transfer+kernel_stddev"] = np.sqrt(avg_variance)
+
+        # Host only without transfer + kernel
+        dfs[i][setting]["average_host"] = dfs[i][setting]["average"] - \
+            dfs[i][setting]["transfer+kernel"]
+
+        # Ignore cl_wide_mem_rw
+        mask = dfs[i][setting]["app_name"].isin(["cl_wide_mem_rw"])
+        dfs[i][setting] = dfs[i][setting][~mask].reset_index(drop=True)
+
+        avg_all_apps = dfs[i][setting].loc[:, "average"].mean()
+        avg_tk_all_apps = dfs[i][setting].loc[:, "transfer+kernel"].mean()
+        avg_host = (dfs[i][setting].loc[:, "average"] - dfs[i][setting].loc[:, "transfer+kernel"]).mean()
+        new_idx = len(dfs[i][setting])
+        dfs[i][setting].loc[new_idx] = {""
+            "app_name": "cl_average",
+            "average": avg_all_apps,
+            "stddev": 0.0, # TODO: average of stddev?
+            "transfer+kernel": avg_tk_all_apps,
+            "average_host": avg_host}
+
+    # app_names = df_u50_slow[setting]["app_name"].values
+    # # Remove cl_
+    # app_names = [s[3:] for s in app_names]
+
+    # # use abbreviations as app names
+    # xlabel_names = []
+    # for app in app_names:
+    #     xlabel_names.append(common.app_names_abb[app])
+
+    # x = np.arange(len(xlabel_names))
+
+    # # Total execution time ----------------------------------------------------------------------------------
+
+    # for i in range(6):
+    #     x_offs = i - 2
+    #     plt.bar(x + x_offs * bar_width, dfs[i][setting]["average"].values,
+    #             hatch=hatches[i % 2], label=labels[i], **bar_args)
+    #     plt.errorbar(x + x_offs * bar_width, dfs[i][setting]["average"].values,
+    #                  yerr=dfs[i][setting]["stddev"].values, **errorbar_args)
+
+    # plt.xticks(x, xlabel_names, rotation=10)
+    # plt.ylabel("Total execution time (s)")
+    # plt.legend(loc='upper left', fancybox=True, shadow=True, ncol=3, bbox_to_anchor=(0, 1.0))
+    # plt.tight_layout()
+    # configure_ax()
+
+    # filename = f"../plots/{setting}/time-xilinx-total.pdf"
+    # print(f"Saving figure to {filename}")
+    # plt.margins(x=0.01, tight=True)
+    # plt.savefig(filename, **savefig_args)
+    # plt.clf()
+
+    # # Data transfer + kernel time ---------------------------------------------------------------------------
+
+    # for i in range(6):
+    #     x_offs = i - 2
+    #     plt.bar(x + x_offs * bar_width, dfs[i][setting]["transfer+kernel"].values,
+    #             hatch=hatches[i % 2], label=labels[i], **bar_args)
+    #     plt.errorbar(x + x_offs * bar_width, dfs[i][setting]["transfer+kernel"].values,
+    #                  yerr=dfs[i][setting]["transfer+kernel_stddev"].values, **errorbar_args)
+
+    # plt.xticks(x, xlabel_names, rotation=10)
+    # plt.ylabel("Total data transfer + kernel time (s)")
+    # plt.legend(loc='upper left', fancybox=True, shadow=True, ncol=3, bbox_to_anchor=(0, 1.0))
+    # plt.tight_layout()
+    # configure_ax()
+
+    # filename = f"../plots/{setting}/time-xilinx-fpga.pdf"
+    # print(f"Saving figure to {filename}")
+    # plt.margins(x=0.01, tight=True)
+    # plt.savefig(filename, **savefig_args)
+    # plt.clf()
+
+# Native vs Proteus plot ------------------------------------------------------------------------------------
+
+bar_width = 0.2
+app_names = df_u50_slow[setting]["app_name"].values
+# Remove cl_
+app_names = [s[3:] for s in app_names]
+# Remove _strm
+for i in range(len(app_names)):
+    if app_names[i] == "wide_mem_rw_strm":
+        app_names[i] = "wide_mem_rw"
+
+xlabel_names = []
+for app in app_names:
+    xlabel_names.append(common.app_names_abb[app])
+
+x = np.arange(len(xlabel_names))
+# dfs = [df_u50_fast, df_u280_fast, df_u280_ddr_fast]
+dfs = [df_u50_fast]
+labels = ["U50 native", "U50 Proteus"]
+
+# Total execution time --------------------------------------------------------------------------------------
+
+# print("Mean of stddevs")
+# fpgas = ["u50", "u280", "u280-ddr"]
+# for i, df in enumerate(dfs):
+#     for setting in ["native", "proteus"]:
+#         mean_stddev = df[setting]["stddev"].mean()
+#         rel = mean_stddev / df[setting].loc[:, "average"].mean()
+#         print(f"{fpgas[i]} {setting}: {mean_stddev}s ({rel * 100}%)")
+
+upper_bar_colors = ["#5783d4", "#de8a54", "#46b097"]
+
+# Arax
+df_u50_fast_arax = pd.read_csv(f"../data/arax/vitis-apps.csv", skipinitialspace=True)
+avg_arax = df_u50_fast_arax[df_u50_fast_arax["avg_time"] > 0].loc[:, "avg_time"].mean()
+new_idx = len(df_u50_fast_arax)
+df_u50_fast_arax.loc[new_idx] = {
+    "app": "average",
+    "avg_time": 0.0, # It doesn't make sense to include the total average if some apps are missing
+    "stddev": 0.0,}
+
+bars = plt.bar(x + bar_width, df_u50_fast_arax["avg_time"].values, hatch=hatches[1],
+        label="U50 Arax", color=common.bar_brown, **bar_args)
+plt.errorbar(x + bar_width, df_u50_fast_arax["avg_time"].values, yerr=df_u50_fast_arax["stddev"].values, **errorbar_args)
+
+arax_overhead = ((df_u50_fast_arax["avg_time"] / df_u50_fast["native"]["average"]) * 100) - 100
+for i, bar in enumerate(bars):
+    if arax_overhead[i] > -100: # Data available
+        plt.text(bar.get_x() + 0.03, bar.get_height() + 0.2, f" {arax_overhead[i]:.1f}%", rotation=90, size=9)
+    else: # No data, put a red X
+        plt.text(bar.get_x() + 0.0072, bar.get_height() + 0.15, f"X", color='red', rotation=0, size=10, fontweight='bold')
+
+# Arax vs Proteus overhead
+arax_overhead_2 = ((df_u50_fast_arax[:10]["avg_time"] / df_u50_fast["native"][:10]["average"]) * 100) - 100
+proteus_overhead_2 = ((df_u50_fast["proteus"][:10]["average"] / df_u50_fast["native"][:10]["average"]) * 100) - 100
+
+print("Mean overheads across all apps that work in Arax (no gmem, wmem):")
+print(f"Arax: {arax_overhead_2.mean()}")
+print(f"Proteus {proteus_overhead_2.mean()}")
+
+x_offs = -1
+# Native
+plt.bar(x + x_offs * bar_width, dfs[0]["native"]["transfer+kernel"].values,
+        hatch=hatches[0], label=labels[0], color=colors[0], **bar_args)
+plt.bar(x + x_offs * bar_width, dfs[0]["native"]["average_host"].values,
+        bottom=dfs[0]["native"]["transfer+kernel"].values, color=upper_bar_colors[0], **bar_args)
+plt.errorbar(x + x_offs * bar_width, dfs[0]["native"]["average"].values,
+                yerr=dfs[0]["native"]["stddev"].values, **errorbar_args)
+x_offs += 1
+
+# Arax at 1, skip
+if x_offs == 1:
+    x_offs += 1
+
+# Proteus
+bars_low = plt.bar(x + x_offs * bar_width, dfs[0]["proteus"]["transfer+kernel"].values,
+                    hatch=hatches[1], label=labels[1], color=colors[0], **bar_args)
+bars_high = plt.bar(x + x_offs * bar_width, dfs[0]["proteus"]["average_host"].values,
+                    bottom=dfs[0]["proteus"]["transfer+kernel"].values,
+                    hatch=hatches[1], color=upper_bar_colors[0], **bar_args)
+proteus_overhead = ((dfs[0]["proteus"]["average"].values /
+                    dfs[0]["native"]["average"].values) * 100) - 100
+for j, (bl, bh) in enumerate(zip(bars_low, bars_high)):
+    plt.text(bl.get_x() + 0.03, bl.get_height() + bh.get_height() + 0.2,
+                f" {proteus_overhead[j]:.1f}%", rotation=90, size=9)
+plt.errorbar(x + x_offs * bar_width, dfs[0]["proteus"]["average"].values,
+                yerr=dfs[0]["proteus"]["stddev"].values, **errorbar_args)
+x_offs += 1
+
+plt.xticks(x, xlabel_names, rotation=0)
+plt.ylabel("Total execution time (s)", fontsize=11)
+handles, labels = plt.gca().get_legend_handles_labels()
+# Put Arax at the end
+order = [1, 2, 0]
+plt.legend([handles[idx] for idx in order],[labels[idx] for idx in order], loc='upper left',
+           fancybox=True, shadow=True, ncol=4, prop={'size': 12}, bbox_to_anchor=(0, 1.2))
+plt.tight_layout()
+configure_ax()
+
+filename = f"../plots/time-xilinx-total.pdf"
+print(f"Saving figure to {filename}")
+plt.margins(x=0.005, tight=True)
+plt.savefig(filename, **savefig_args)
+plt.show()
+plt.clf()
+
+# Data transfer + kernel time -------------------------------------------------------------------------------
+
+# x_offs = -2
+# for i in range(3):
+#     # Native
+#     plt.bar(x + x_offs * bar_width, dfs[i]["native"]["transfer+kernel"].values,
+#             hatch=hatches[0], label=labels[2 * i], **bar_args)
+#     plt.errorbar(x + x_offs * bar_width, dfs[i]["native"]["transfer+kernel"].values,
+#                  yerr=dfs[i]["native"]["transfer+kernel_stddev"].values, **errorbar_args)
+#     x_offs += 1
+#     # Proteus
+#     bars = plt.bar(x + x_offs * bar_width, dfs[i]["proteus"]["transfer+kernel"].values,
+#                    hatch=hatches[1], label=labels[2 * i + 1], **bar_args)
+#     proteus_overhead = ((dfs[i]["proteus"]["transfer+kernel"].values /
+#                          dfs[i]["native"]["transfer+kernel"].values) * 100) - 100
+#     for j, b in enumerate(bars):
+#         plt.text(b.get_x() + 0.19 * bar_width, b.get_height() + 1.5,
+#                  f"{proteus_overhead[j]:.1f}%", rotation=90, size=8)
+#     plt.errorbar(x + x_offs * bar_width, dfs[i]["proteus"]["transfer+kernel"].values,
+#                  yerr=dfs[i]["proteus"]["transfer+kernel_stddev"].values, **errorbar_args)
+#     x_offs += 1
+
+# plt.xticks(x, xlabel_names, rotation=10)
+# plt.ylabel("Total data transfer + kernel time (s)")
+# plt.legend(loc='upper left', fancybox=True, shadow=True, ncol=3, bbox_to_anchor=(0, 1.0))
+# plt.tight_layout()
+# configure_ax()
+
+# filename = f"../plots/time-xilinx-fpga.pdf"
+# print(f"Saving figure to {filename}")
+# plt.margins(x=0.01, tight=True)
+# plt.savefig(filename, **savefig_args)
+# plt.clf()
