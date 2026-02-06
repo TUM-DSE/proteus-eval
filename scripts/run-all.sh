2040c4e2c7fdcec20b706ac7a94c6f77c667b1bd
Felix Gust <gustifix@users.noreply.github.com>
2026-02-06 13:24:42 +0100

    Add missing scripts to run-all.sh

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -5,14 +5,21 @@ set -xeuo pipefail
 script_dir=$(dirname "$(readlink -f "$0")")
 cd "$script_dir"
 
+./estimate-intel-performance.py
 ./scheduler.py # scheduler produces a csv file read by speedup, keep this order
 ./speedup.py
-./time-compute.py
+./sched_preprocess.py # keep the order of sched_* too
+./sched_scoring.py
+./sched_plot.py
+./time-multi-fpga.py
+./time-xilinx.py
 ./time-breakdown.py
 ./time-intel.py
 ./time-rosetta-xilinx.py
 ./throughput-motivation.py
 ./throughput.py
+./overheads.py
+./time-memory.py
 ./memory-channels-total.py
 ./memory-channels-fpga.py
 ./oversub.py
